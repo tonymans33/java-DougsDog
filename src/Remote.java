73bbc70278cc90391c7c9464c844c97fb9dddca3
tonymans33 <tonymansour66@yahoo.com>
2021-01-12 16:03:19 +0200

    adding remote class

--- src/Remote.java
+++ src/Remote.java
@@ -1,2 +1,22 @@
+// Remote class to make the man and wife control the door remotely
 public class Remote {
+
+    private DogDoor door;
+
+    // Remote constructor to give the remote a door to control
+    public Remote(DogDoor door){
+        this.door = door;
+    }
+
+    // A method to control pressing the button if will open or close the door
+    public void pressButton(){
+        System.out.println("Pressing the remote control button ...");
+        if(door.isOpen()){
+            door.close();
+        }else {
+            door.open();
+        }
+
+    }
+
 }
